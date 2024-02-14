38c37b41f024d73f3d5f343ed2e7cec909e22a7f
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-14 09:52:24 +0900

    style(lint): prettier format

--- docs/components/Header.vue
+++ docs/components/Header.vue
@@ -22,7 +22,11 @@ const { header } = useAppConfig()
     </template>
 
     <template #right>
-      <UDocsSearchButton v-if="header?.search" :label="null" class="lg:hidden" />
+      <UDocsSearchButton
+        v-if="header?.search"
+        :label="null"
+        class="lg:hidden"
+      />
 
       <UColorModeButton v-if="header?.colorMode" />
 
