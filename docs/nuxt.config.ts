98ce81238a5a6c6622ac2ac7c9314021d5e0b220
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-14 10:56:20 +0900

    feat: create docs on Azure Static Web Apps (#3)
    - init /docs
      - `nuxi init -t github:nuxt-ui-pro/docs docs`
      - install internal module
      - `preset:azure`
      - merge lint config
    - remove `/playground` folder
    - Setup Azure Static Web Apps CI/CD

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  nitro: { preset: 'azure' },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxthq/studio',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/google-fonts',
+    'nuxt-og-image',
+    '../src/module',
+  ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': components => {
+      const globals = components.filter(c =>
+        ['UButton', 'UIcon'].includes(c.pascalName)
+      )
+
+      globals.forEach(c => (c.global = true))
+    },
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+  },
+  // Fonts
+  fontMetrics: {
+    fonts: ['DM Sans'],
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: {
+      'DM+Sans': [400, 500, 600, 700],
+    },
+  },
+  routeRules: {
+    '/api/search.json': { prerender: true },
+  },
+  devtools: {
+    enabled: true,
+  },
+  typescript: {
+    strict: false,
+  },
+  myModule: {},
+})
