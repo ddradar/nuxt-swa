9116dbe1447853957cc42ceba8e018918d3f7f3c
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-14 15:26:58 +0900

    build: use Node.js 18

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
-  nitro: { preset: 'azure' },
+  nitro: {
+    preset: 'azure',
+    azure: { config: { platform: { apiRuntime: 'node:18' } } },
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/ui',
