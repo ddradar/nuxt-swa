94343c510cea71e6cb79ee7a6189d220efd4ee03
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-18 10:05:20 +0900

    cchore: add comment

--- packages/nuxt-swa/src/module.ts
+++ packages/nuxt-swa/src/module.ts
@@ -16,6 +16,7 @@ type KnownIdentityProvider =
 export interface SWAModuleOptions {
   /**
    * Authentication Provider list to login your app.
+   * If your app does not use built-in Authentication/Authorization, please set this to `[]`.
    * @default ['aad', 'github']
    */
   authProviders: (KnownIdentityProvider | string)[]
@@ -42,12 +43,14 @@ export default defineNuxtModule<SWAModuleOptions>({
     const resolver = createResolver(import.meta.url)
 
     if (options.authProviders.length) {
+      // Use Authentication/Authorization features
       addType('IdentityProvider', true)
       addImports({
         name: 'useEasyAuth',
         from: resolver.resolve('runtime/composables/useEasyAuth'),
       })
     }
+    // Add `nitro.azure.config` Typing
     addType('UserRole', true)
     addType('AzureOptions', true)
 
