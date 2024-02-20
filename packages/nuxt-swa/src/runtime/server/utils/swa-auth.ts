64c7e8862902e366a782b40555b0d160464ba188
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-20 22:55:20 +0900

    test: add unit test for server/utils

--- packages/nuxt-swa/src/runtime/server/utils/swa-auth.ts
+++ packages/nuxt-swa/src/runtime/server/utils/swa-auth.ts
@@ -1,4 +1,4 @@
-import type { H3Event } from 'h3'
+import { type H3Event, getHeader } from 'h3'
 
 import { _clientPrincipalHeader } from '../../../constants'
 import { parseClientPrincipal } from '../../../utils'
--- packages/nuxt-swa/test/unit/constants.ts
+++ packages/nuxt-swa/test/unit/constants.ts
@@ -1,7 +1,7 @@
 export const _clientPrincipal = {
-  identityProvider: 'github',
+  identityProvider: 'github' as const,
   userId: 'd75b260a64504067bfc5b2905e3b8182',
   userDetails: 'username',
-  userRoles: ['anonymous', 'authenticated'],
+  userRoles: ['anonymous' as const, 'authenticated' as const],
   claims: [{ typ: 'name', val: 'Azure Static Web Apps' }],
-} as const
+}
