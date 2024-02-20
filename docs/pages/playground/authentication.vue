5d3082d12294226281f9a1ae2c84bd45944d7ce8
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-20 10:02:37 +0900

    refactor(docs): create usePageContent composable

--- docs/pages/playground/authentication.vue
+++ docs/pages/playground/authentication.vue
@@ -1,36 +1,7 @@
 <script setup lang="ts">
-import { withoutTrailingSlash } from 'ufo'
-
 definePageMeta({ layout: 'docs' })
 
-const { seo } = useAppConfig()
-const { data: page } = await useAsyncData('/playground/authentication', () =>
-  queryContent('/playground/authentication').findOne()
-)
-if (!page.value) {
-  throw createError({
-    statusCode: 404,
-    statusMessage: 'Page not found',
-    fatal: true,
-  })
-}
-
-const { data: surround } = await useAsyncData(
-  `/playground/authentication-surround`,
-  () =>
-    queryContent()
-      .where({ _extension: 'md', navigation: { $ne: false } })
-      .only(['title', 'description', '_path'])
-      .findSurround(withoutTrailingSlash('/playground/authentication'))
-)
-useSeoMeta({
-  titleTemplate: `%s - ${seo?.siteName}`,
-  title: page.value.title,
-  ogTitle: `${page.value.title} - ${seo?.siteName}`,
-  description: page.value.description,
-  ogDescription: page.value.description,
-})
-const headline = computed(() => findPageHeadline(page.value!))
+const { headline, page, surround } = await usePageContent()
 
 // Playground Block
 const { isLoggedIn, clientPrincipal } = await useEasyAuth()
@@ -40,9 +11,9 @@ const json = computed(() => JSON.stringify(clientPrincipal.value, undefined, 2))
 <template>
   <UPage>
     <UPageHeader
-      :title="page?.title"
-      :description="page?.description"
-      :links="page?.links"
+      :title="page.title"
+      :description="page.description"
+      :links="page.links"
       :headline="headline"
     />
     <UPageBody prose>
