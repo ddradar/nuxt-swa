d27b2e7704702ad5e7452c0bdb27b49fd0cc6338
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-22 02:42:11 +0000

    refactor(docs): move duplicated logic to layout

--- docs/layouts/docs.vue
+++ docs/layouts/docs.vue
@@ -1,6 +1,48 @@
 <script setup lang="ts">
 import type { NavItem } from '@nuxt/content/dist/runtime/types'
+import { withoutTrailingSlash } from 'ufo'
 
+const { toc, seo } = useAppConfig()
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () =>
+  queryContent(route.path).findOne()
+)
+if (!page.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page not found',
+    fatal: true,
+  })
+}
+
+// Page headline and surround
+const headline = computed(() => findPageHeadline(page.value!))
+const { data: surround } = await useAsyncData(`${route.path}-surround`, () =>
+  queryContent()
+    .where({ _partial: false, navigation: { $ne: false } })
+    .only(['title', 'description', '_path'])
+    .findSurround(withoutTrailingSlash(route.path))
+)
+
+useSeoMeta({
+  titleTemplate: `%s - ${seo?.siteName}`,
+  title: page.value.title,
+  ogTitle: `${page.value.title} - ${seo?.siteName}`,
+  description: page.value.description,
+  ogDescription: page.value.description,
+})
+
+const links = computed(() =>
+  [
+    toc?.bottom?.edit && {
+      icon: 'i-heroicons-pencil-square',
+      label: 'Edit this page',
+      to: `${toc.bottom.edit}/${page?.value?._file}`,
+      target: '_blank',
+    },
+    ...(toc?.bottom?.links || []),
+  ].filter(Boolean)
+)
 const navigation = inject<Ref<NavItem[]>>('navigation')
 </script>
 
@@ -13,7 +55,35 @@ const navigation = inject<Ref<NavItem[]>>('navigation')
         </UAside>
       </template>
 
-      <slot />
+      <UPageHeader
+        :title="page?.title"
+        :description="page?.description"
+        :links="page!.links"
+        :headline="headline"
+      />
+
+      <UPageBody prose>
+        <slot></slot>
+
+        <hr v-if="surround?.length" />
+
+        <UDocsSurround :surround="surround!" />
+      </UPageBody>
+
+      <template v-if="page?.toc !== false" #right>
+        <UDocsToc :title="toc?.title" :links="page!.body?.toc?.links">
+          <template v-if="toc?.bottom" #bottom>
+            <div
+              class="hidden lg:block space-y-6"
+              :class="{ '!mt-6': page.body?.toc?.links?.length }"
+            >
+              <UDivider v-if="page.body?.toc?.links?.length" type="dashed" />
+
+              <UPageLinks :title="toc.bottom.title" :links="links" />
+            </div>
+          </template>
+        </UDocsToc>
+      </template>
     </UPage>
   </UContainer>
 </template>
--- docs/pages/playground/middleware.vue
+++ docs/pages/playground/middleware.vue
@@ -1,54 +1,9 @@
 <script setup lang="ts">
-import { withoutTrailingSlash } from 'ufo'
-
 definePageMeta({ layout: 'docs', allowedRoles: 'authenticated' })
-
-const { seo } = useAppConfig()
-const route = useRoute()
-const { data: page } = await useAsyncData(route.path, () =>
-  queryContent(route.path).findOne()
-)
-if (!page.value) {
-  throw createError({
-    statusCode: 404,
-    statusMessage: 'Page not found',
-    fatal: true,
-  })
-}
-
-// Page headline and surround
-const headline = computed(() => findPageHeadline(page.value!))
-const { data: surround } = await useAsyncData(`${route.path}-surround`, () =>
-  queryContent()
-    .where({ navigation: { $ne: false } })
-    .only(['title', 'description', '_path'])
-    .findSurround(withoutTrailingSlash(route.path))
-)
-
-useSeoMeta({
-  titleTemplate: `%s - ${seo?.siteName}`,
-  title: page.value.title,
-  ogTitle: `${page.value.title} - ${seo?.siteName}`,
-  description: page.value.description,
-  ogDescription: page.value.description,
-})
 </script>
 
 <template>
-  <UPage>
-    <UPageHeader
-      :title="page!.title"
-      :description="page!.description"
-      :links="page!.links"
-      :headline="headline"
-    />
-    <UPageBody prose>
-      <Callout icon="i-heroicons-lock-open-20-solid" color="blue">
-        Private Page!
-      </Callout>
-
-      <hr v-if="surround?.length" />
-      <UDocsSurround :surround="surround!" />
-    </UPageBody>
-  </UPage>
+  <Callout icon="i-heroicons-lock-open-20-solid" color="blue">
+    Private Page!
+  </Callout>
 </template>
