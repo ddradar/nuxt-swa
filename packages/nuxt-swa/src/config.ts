220dc04ad87c78d64e53e928ff093b5268b5977e
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-29 11:45:03 +0900

    chore: move pagemeta config

--- packages/nuxt-swa/src/config.ts
+++ packages/nuxt-swa/src/config.ts
@@ -83,3 +83,19 @@ export interface ModulePublicRuntimeConfig {
     /** GraphQL endpoint */ graphql: string
   }
 }
+
+type SpecifiedRole = Exclude<UserRole, 'anonymous'>
+
+declare module '#app' {
+  interface PageMeta {
+    /** List of roles required to view this page */
+    allowedRoles?: SpecifiedRole | SpecifiedRole[]
+  }
+}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    /** List of roles required to view this page */
+    allowedRoles?: SpecifiedRole | SpecifiedRole[]
+  }
+}
