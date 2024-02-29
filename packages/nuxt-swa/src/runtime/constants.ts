92f5dd7d623b20662908e7b0444a2df9925e2cfe
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-29 09:37:16 +0900

    Revert "refactor: const header string"
    This reverts commit 6172394280d3b7dfb624cabc4942285f767b4c36.

--- packages/nuxt-swa/src/runtime/constants.ts
+++ packages/nuxt-swa/src/runtime/constants.ts
@@ -2,7 +2,6 @@
 export const _authBasePath = '/.auth'
 export const _dataApiBasePath = '/data-api'
 export const _clientPrincipalHeader = 'x-ms-client-principal'
-export const _originalUrlHeader = 'x-ms-original-url'
 export const _preConfiguredProviders = ['aad' as const, 'github' as const]
 export const _knownIdentityProviders = [
   'apple',
--- packages/nuxt-swa/src/runtime/server/proxy.ts
+++ packages/nuxt-swa/src/runtime/server/proxy.ts
@@ -1,10 +1,8 @@
 import { defineEventHandler, getHeader, proxyRequest } from 'h3'
 import { joinURL, parseURL } from 'ufo'
 
-import { _originalUrlHeader } from '../constants'
-
 export default defineEventHandler(event => {
-  const originalUri = parseURL(getHeader(event, _originalUrlHeader))
+  const originalUri = parseURL(getHeader(event, 'x-ms-original-url'))
   const domain = `${originalUri.protocol}//${originalUri.host}`
 
   return proxyRequest(event, joinURL(domain, event.path))
