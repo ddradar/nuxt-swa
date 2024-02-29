6172394280d3b7dfb624cabc4942285f767b4c36
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-02-29 09:21:34 +0900

    refactor: const header string

--- packages/nuxt-swa/src/runtime/server/proxy.ts
+++ packages/nuxt-swa/src/runtime/server/proxy.ts
@@ -1,8 +1,10 @@
 import { defineEventHandler, getHeader, proxyRequest } from 'h3'
 import { joinURL, parseURL } from 'ufo'
 
+import { _originalUrlHeader } from '../constants'
+
 export default defineEventHandler(event => {
-  const originalUri = parseURL(getHeader(event, 'x-ms-original-url'))
+  const originalUri = parseURL(getHeader(event, _originalUrlHeader))
   const domain = `${originalUri.protocol}//${originalUri.host}`
 
   return proxyRequest(event, joinURL(domain, event.path))
