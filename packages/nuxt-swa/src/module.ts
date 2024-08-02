57c8fcaaf31bae1a57b09517579e1a5eecd1de34
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-08-02 16:12:23 +0900

    fix(types): move back type defs

--- packages/nuxt-swa/src/module.ts
+++ packages/nuxt-swa/src/module.ts
@@ -62,6 +62,10 @@ export default defineNuxtModule<ModuleOptions>({
       getContents: ({ options }) => generateTypeDefinition(options),
       options,
     })
+    nuxt.hook('prepare:types', ({ references }) => {
+      references.push({ path: `types/${packageName}.d.ts` })
+      references.push({ path: resolve('./runtime/types.d.ts') })
+    })
 
     // Proxy Azure SWA built-in API
     addServerHandler({
@@ -442,38 +446,6 @@ export function generateTypeDefinition(options: ModuleOptions) {
  * @see https://learn.microsoft.com/azure/static-web-apps/authentication-custom#manage-roles
  */
 type UserRole = ${['anonymous', 'authenticated', ...options.customRoles].map(s => `'${s}'`).join(' | ')}
-
-interface Claim {
-  typ: string
-  val: string
-}
-
-/**
- * User-identifiable information to your app.
- * @see https://learn.microsoft.com/azure/static-web-apps/user-information?tabs=javascript#client-principal-data
- */
-export interface ClientPrincipal {
-  /** The name of the identity provider. */
-  identityProvider: IdentityProvider
-  /**
-   * An Azure Static Web Apps-specific unique identifier for the user.
-   * - The value is unique on a per-app basis. For instance, the same user returns a different \`userId\` value on a different Static Web Apps resource.
-   * - The value persists for the lifetime of a user. If you delete and add the same user back to the app, a new \`userId\` is generated.
-   */
-  userId: string
-  /**
-   * Username or email address of the user.
-   * Some providers return the user's email address, while others send the user handle.
-   */
-  userDetails: string
-  /** An array of the user's assigned roles. */
-  userRoles: UserRole[]
-  /**
-   * An array of claims returned by your custom authentication provider.
-   * Only accessible in the direct-access endpoint (/.auth/me).
-   */
-  claims?: Claim[]
-}
 
 // Auth Feature
 ${
